7366c67ed9bc734856fe9c6c0f55aa2c36091932
kimdimka <kimdimka@yandex.ru>
2018-10-22 16:54:29 +1000

    22102018_доделать проерку на аттрибут инвалид

--- TicketSalePoint/wwwroot/js/HallPlaces.js
+++ TicketSalePoint/wwwroot/js/HallPlaces.js
@@ -1,83 +1,95 @@
-﻿$(document).ready(function () {
-    
-    $("td[isSold=1]").css("backgroundColor", "orange");
-    $("td[isSold=1]").removeAttr('onclick');
-    
-
-    $.validator.addClassRules({
-        orderRowId1: {
-            required: true,
-            digits: true,
-            min: 0,
-            max: 100
-        },
-        orderRowId2: {
-            required: true,
-            digits: true,
-            min: 0,
-            max: 100
+﻿var isSubmitAllowed;
+function validatePass() {
+    $('form input').each(function (index, elem)
+    {
+        if (elem.className == 'orderRowId1' || elem.className == 'orderRowId2')
+        {
+            $(elem).rules("add",
+            {
+                min: 10,
+                max: 20,
+                required: true,
+                messages:
+                {
+                    max: "больше 20",
+                    min: "меньше 10"
+                }
+            })
         }
     });
-
-
-        $('#OrderForm').validate({
-            rules: {
-                kol: {
-                    required: true,
-                    digits: true,
-                    min: 1,
-                    max: 30
-                }
-                , kolChildren: {
-                    required: true,
-                    min: 1,
-                    max: 30
-                }
-                , fIO: {
-                    required: true
-                }
-                , phone: {
-                    required: true
-                }
-                , fIOChildren: {
-                    required: true
-                }
-                , age: {
-                    required: true
-                }
-
-            },
-            highlight: function (element, errorClass) {
+}
+$(document).ready(function ()
+{
+    $("td[isSold=1]").css("backgroundColor", "orange");
+    $("td[isSold=1]").removeAttr('onclick');
+    $('form').validate(
+        {
+            highlight: function (element, errorClass)
+            {
                 $(element).add($(element).parent()).addClass("invalidElem");
             },
-            unhighlight: function (element, errorClass) {
+            unhighlight: function (element, errorClass)
+            {
                 $(element).add($(element).parent()).removeClass("invalidElem");
             },
             errorElement: "tr",
             errorClass: "errorMessages",
             validClass: "success"
         });
-
-
+    validatePass();
+    $('form').submit(function()
+    {
+        var areAllFieldsValid = true;
+        $('form input').each(function(e)
+        {
+            $('form input').validate().element($(this));
+            var attr = $(this).attr('aria-invalid');
+            if (typeof attr !== typeof undefined)
+            {
+                if ($(this).attr('aria-invalid') && (this.type != 'hidden'))
+                    areAllFieldsValid = false;
+            }
+        });
+        alert(areAllFieldsValid);
+        return (areAllFieldsValid);
+    });
 
 });  //document.Ready()
 
-function ticketsNumberChange() {
+function ticketsNumberChange()
+{
     var ticketNumber = $("#ticketNumber").val();
     $(".OrderTableTemplate tr:gt(2)").remove();
-    for (i = 1; i <= ticketNumber-1; i++) {
+    for (i = 1; i <= ticketNumber - 1; i++)
+    {
         var newOrderTemplateRow = $(".orderRowTemplate").clone().removeClass("orderRowTemplate");
         newOrderTemplateRow.appendTo(".OrderTableTemplate").fadeIn();
     }
-} 
+    validatePass();
+    $('form input').each(function (e)
+    {
+            $('form').validate().element($(this));
+    });
 
-function ticketsNumberChangeChildren() {
+}
+
+function ticketsNumberChangeChildren()
+{
     var ticketNumber = $("#ticketNumberChildren").val();
     $(".OrderTableTemplateChildren tr:gt(2)").remove();
-    for (i = 1; i <= ticketNumber - 1; i++) {
+    for (i = 1; i <= ticketNumber - 1; i++)
+    {
         var newOrderTemplateRow = $(".orderRowTemplateChildren").clone().removeClass("orderRowTemplateChildren");
         newOrderTemplateRow.appendTo(".OrderTableTemplateChildren").fadeIn();
     }
+
+    validatePass();
+
+    $('form input').each(function (e)
+    {
+        $('form').validate().element($(this));
+    });
+
 }
 
 
