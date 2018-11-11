311a0d4173f96aed8801f70c7ac7c0cce88020a7
shtolce <kimdimka@yandex.ru>
2018-11-11 13:54:24 +1000

    1

---
+++ TicketSalePoint/Data/Migrations/20181108101105_dw11de.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TicketSalePoint.Data.Migrations
+{
+    public partial class dw11de : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- TicketSalePoint/wwwroot/js/site.js
+++ TicketSalePoint/wwwroot/js/site.js
@@ -2,7 +2,19 @@
 
 
 function sellTicketForm(e) {
-    $('body').load('https://ticketsalepoint20181107014415.azurewebsites.net/home/sellForm?id=' + e.target.id.trim() + '&curEmissionId=' + e.target.getAttribute("curEmissionId"));
+   // $('body').load('/home/sellForm?id=' + e.target.id.trim() + '&curEmissionId=' + e.target.getAttribute("curEmissionId"));
+    
+    var form = document.createElement("form");
+    form.setAttribute("method", "GET");
+    form.setAttribute("action", '/home/sellForm?id=' + e.target.id.trim() + '&curEmissionId=' + e.target.getAttribute("curEmissionId"));
+    document.body.appendChild(form);
+
+    var filters = document.createElement('input');
+    filters.setAttribute('id', e.target.id.trim());
+    filters.setAttribute('curEmissionId', e.target.getAttribute("curEmissionId"));
+    form.appendChild(filters);
+    form.submit();
+
 }
 
 
